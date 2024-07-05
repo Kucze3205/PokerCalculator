eba280d2cdb98b7f54e6cda2c214144f944c80e8
Kucze3205 <kuczerowski2005@gmail.com>
2024-07-05 17:41:00 +0200

    Visual actualization

---
+++ Converters/WidthToFontSizeConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace PokerCalculatorWPF.Converters
+{
+    public class WidthToFontSizeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double width && width > 200)
+            {               
+                return width / 20 - 50;
+            }
+            return 20; // Default font size
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
